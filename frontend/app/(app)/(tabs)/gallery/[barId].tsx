5fdc8d652a818f298617c725593dcacb855a2175
Geni Williams <willgen@iastate.edu>
2025-11-16 18:11:37 -0600

    Download functionality added

--- frontend/app/(app)/(tabs)/gallery/[barId].tsx
+++ frontend/app/(app)/(tabs)/gallery/[barId].tsx
@@ -1,11 +1,14 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, Image, FlatList, ActivityIndicator, 
-  StyleSheet, Dimensions, Modal, TouchableOpacity, } from "react-native";
+  StyleSheet, Dimensions, Alert, TouchableOpacity, } from "react-native";
 import { useLocalSearchParams, useRouter } from "expo-router";
-import type { Photo } from "@/services/photosService"
+import type { Photo } from "@/services/photosService";
 import { getPhotosByAlbumUri } from "@/services/photosService";
 import { Theme } from "@/constants/theme";
 import ImageViewing from "react-native-image-viewing";
+import { FontAwesome } from "@expo/vector-icons";
+import { File, Directory, Paths} from 'expo-file-system';
+import * as MediaLibrary from "expo-media-library";
 
 const windowWidth = Dimensions.get("window").width;
 const PHOTO_SIZE = windowWidth / 3;
@@ -17,6 +20,7 @@ export default function BarPhotosScreen() {
   const [photos, setPhotos] = useState<Photo[]>([]);
   const [loading, setLoading] = useState(true);
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [viewerIndex, setViewerIndex] = useState(0);
   const [isViewerVisible, setViewerVisible] = useState(false);
 
   // load album
@@ -37,6 +41,74 @@ export default function BarPhotosScreen() {
 
   const imageSources = photos.map((p) => ({ uri: p.image.uri }));
 
+  // Helper function that handles enlarged photo downloads
+  const handleDownload = async () => {
+    try {
+      const uri = photos[viewerIndex].image.uri;
+      if (!uri) {
+        Alert.alert("Invalid image", "This image cannot be downloaded.");
+        return;
+      }
+
+      // Ask permission
+      const { status } = await MediaLibrary.requestPermissionsAsync();
+      if (status !== "granted") {
+        Alert.alert("Permission Needed", "Allow photo access to save images.");
+        return;
+      }
+
+      // Create destination directory
+      const folder = new Directory(Paths.cache, 'AmesAfterDark');
+      if (!folder.exists) {
+        await folder.create();
+      }
+
+      // Download file
+      const downloadedFile = await File.downloadFileAsync(uri, folder);
+
+      // Save to library
+      await MediaLibrary.saveToLibraryAsync(downloadedFile.uri);
+      Alert.alert("Saved", "Photo saved to your camera roll!");
+    } catch (err) {
+      console.log(err);
+      Alert.alert("Error", "Could not download image.");
+    }
+  };
+
+  // Helped function that handles long press grid photo downloads
+  const handleGridDownload = async (index: number) => {
+    try {
+      const uri = photos[index].image.uri;
+      if (!uri) {
+        Alert.alert("Invalid image", "This image cannot be downloaded.");
+        return;
+      }
+
+      // Ask permission
+      const { status } = await MediaLibrary.requestPermissionsAsync();
+      if (status !== "granted") {
+        Alert.alert("Permission Needed", "Allow photo access to save images.");
+        return;
+      }
+
+      // Create destination directory
+      const folder = new Directory(Paths.cache, 'AmesAfterDark');
+      if (!folder.exists) {
+        await folder.create();
+      }
+
+      // Download file
+      const downloadedFile = await File.downloadFileAsync(uri, folder);
+
+      // Save to library
+      await MediaLibrary.saveToLibraryAsync(downloadedFile.uri);
+      Alert.alert("Saved", "Photo saved to your camera roll!");
+    } catch (err) {
+      console.log(err);
+      Alert.alert("Error", "Could not download image.");
+    }
+  };
+
   return (
     <View style={styles.container}>
       {/* Bar name header */}
@@ -53,7 +125,8 @@ export default function BarPhotosScreen() {
         numColumns={3}
         renderItem={({ item, index }) => (
           <TouchableOpacity onPress={() => {
-          setCurrentIndex(index); setViewerVisible(true);}}>
+          setCurrentIndex(index); setViewerVisible(true); setViewerIndex(index);}}
+          onLongPress={() => handleGridDownload(index)} delayLongPress={400}>
             <Image source={item.image} style={styles.photo} resizeMode="cover" />
           </TouchableOpacity>
         )}
@@ -64,12 +137,17 @@ export default function BarPhotosScreen() {
       <ImageViewing images={imageSources} imageIndex={currentIndex}
         visible={isViewerVisible} onRequestClose={() => setViewerVisible(false)}
         swipeToCloseEnabled={true} doubleTapToZoomEnabled={true}
-        // Back button
+        onImageIndexChange={(index) => setViewerIndex(index)}
         HeaderComponent={() => (
           <View style={styles.viewerHeader}>
             <TouchableOpacity onPress={() => {setViewerVisible(false);}}>
               <Text style={styles.viewerBackArrow}>←</Text>
             </TouchableOpacity>
+            <View style={styles.viewerHeaderRight}>
+              <TouchableOpacity onPress={handleDownload}>
+                <FontAwesome name="download" style={styles.downloadIcon} />
+              </TouchableOpacity>
+            </View>
           </View>
         )}
       />
@@ -110,13 +188,25 @@ const styles = StyleSheet.create({
   },
   viewerHeader: {
     position: "absolute",
-    top: 45,
-    left: 25,
-    zIndex: 20,
+    top: 50,
+    left: 0,
+    right: 0,
+    zIndex: 50,
+    paddingHorizontal: 16,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  viewerHeaderRight: {
+    flexDirection: "row",
+    alignItems: "center",
   },
   viewerBackArrow: {
     fontSize: 34,
     color: Theme.dark.primary,
-    fontWeight: "700",
+  },
+  downloadIcon: {
+    fontSize: 34,
+    color: Theme.dark.primary,
   },
 });
