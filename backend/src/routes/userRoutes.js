60c4f7fdfdb567153c0270ab2d1154711470e4a8
ncouture@iastate.edu <ncouture@iastate.edu>
2026-02-19 23:21:53 -0600

    Added some tests

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,18 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const checkJwt = require('../middleware/authMiddleware');
 
 router.get('/:userId/friends', userController.getUserFriends);
-
-
 
 // CRUD routes
 router.get('/', userController.getUsers);         // Read all
 router.get('/:id', userController.getUserById);  // Read one
 router.put('/:id', userController.updateUserLimited);   // Update
-router.get('/auth/status', userController.checkUserStatus); // Check user status
-router.post('/auth/register', userController.completeUserRegistration); // Create user with signup info
-
+// router.get('/auth/status', userController.checkUserStatus); // Check user status
+// router.post('/auth/register', userController.completeUserRegistration); // Create user with signup info
 
 // TEMP_AUTH_START - Remove when re-enabling Auth0
 router.post('/signup', userController.createUser);
