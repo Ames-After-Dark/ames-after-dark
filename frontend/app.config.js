a52accf92a36b5985429c0d6b807dfc6ffa2b6f4
ncouture@iastate.edu <ncouture@iastate.edu>
2026-02-28 15:07:20 -0600

    checkpoint to fix things

--- frontend/app.config.js
+++ frontend/app.config.js
@@ -9,7 +9,7 @@ export default {
     icon: "./assets/images/icon.png",
     scheme: "com.anonymous.amesafterdark",
     userInterfaceStyle: "automatic",
-    newArchEnabled: true,
+    newArchEnabled: false,
 
     ios: { supportsTablet: true },
 
