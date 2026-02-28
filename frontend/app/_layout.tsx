45d70ba07c573790b8c3ba29b73335359d437e61
ncouture@iastate.edu <ncouture@iastate.edu>
2026-02-28 14:47:27 -0600

    still broken right after sdk add

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,15 +1,14 @@
 import { Slot } from 'expo-router'
-// import { Auth0Provider } from "react-native-auth0"
-// import { AuthProvider } from "@/hooks/use-auth"
-// import { config } from '@/auth0.config'
+import { Auth0Provider } from "react-native-auth0"
+import { AuthProvider } from "@/hooks/use-auth"
+import { config } from '@/auth0.config'
 
 export default function RootLayout() {
-  // return (
-  //     <Auth0Provider domain={config.domain} clientId={config.clientId}>
-  //       <AuthProvider>
-  //           <Slot />
-  //       </AuthProvider>
-  //     </Auth0Provider>
-  // );
-  return (< Slot />);
+  return (
+      <Auth0Provider domain={config.domain} clientId={config.clientId}>
+        <AuthProvider>
+            <Slot />
+        </AuthProvider>
+      </Auth0Provider>
+  );
 }
