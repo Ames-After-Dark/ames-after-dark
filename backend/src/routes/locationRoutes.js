1e27288d0634703d99ed4560689f7ef5efa817a2
Nathan Krieger <nathankrieger20@gmail.com>
2026-04-15 09:46:21 -0500

    developer perms

--- backend/src/routes/locationRoutes.js
+++ backend/src/routes/locationRoutes.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const locationController = require('../controllers/locationController');
+const { checkJwt, optionalJwt } = require('../middleware/authMiddleware');
 
 // GET open locations with optional query param ?utc=...
 router.get('/open', locationController.getOpenLocations);
 // Get locations with their hours included
 router.get('/with-hours', locationController.getLocationsWithHours);
 
-router.get('/admin/:id', locationController.getLocationsByAdminId);
+router.get('/admin/:id',checkJwt, locationController.getLocationsByAdminId);
+
+router.get('/developer/:id', checkJwt, locationController.getLocationsByDeveloperId);
 
 router.get('/views/:id', locationController.getTotalLocationViewsById);
 
